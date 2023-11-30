6982a67cb8445a508c28419dd11b16a2f63dc8d5
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-30 06:29:45 +0100

    add food_layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'apps#index'
   devise_for :users, controllers: {sessions: 'users/sessions',registrations: "users/registrations"}
   get 'up' => 'rails/health#show', as: :rails_health_check
+  resources :foods, except: [:update]
 end
