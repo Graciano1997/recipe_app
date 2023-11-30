211d89165f2962176b6f1a30c5c6791fc47053de
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-30 11:22:35 +0100

    add food functionality

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,6 +2,6 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   validates :name, presence: true
-  validates :price, numericality:{greather_than_or_equal_to: 0}
-  validates :quantity, numericality:{ only_integer: true, greather_than_or_equal_to: 0}
+  validates :price, numericality: { greather_than_or_equal_to: 0 }
+  validates :quantity, numericality: { only_integer: true, greather_than_or_equal_to: 0 }
 end
