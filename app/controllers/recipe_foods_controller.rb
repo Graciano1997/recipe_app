05fffd5f1392114f8a282c909bcab777f59909e5
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-12-01 10:19:51 +0100

    update revipe_food value

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -2,14 +2,27 @@ class RecipeFoodsController < ApplicationController
   def new
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = RecipeFood.new
-    @user=User.find(current_user.id)
+    @foods=Food.all
   end
 
   def index
   end
 
   def create
-    RecipeFood.create!(quantity: params[:recipe_food]["quantity"],food_id:params[:recipe_food]["food_id"],recipe_id:params[:recipe_id])
+    @recipe_food=RecipeFood.where(food_id: params[:recipe_food]["food_id"]).first
+    if @recipe_food
+      @recipe_food.quantity+=params[:recipe_food]["quantity"].to_i
+      @recipe_food.save
+    else
+      @recipe=RecipeFood.new(recipe_food_params)
+     @recipe.save
+    end
     redirect_to recipe_path(params[:recipe_id].to_i)
+  end
+
+  private
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:quantity, :food_id,:recipe_id)
   end
 end
