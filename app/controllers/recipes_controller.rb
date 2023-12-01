a47a73325158c7c8f28b14a48336cd7acf7e70b3
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-12-01 10:31:13 +0100

    fix linter issues

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,23 +1,18 @@
 class RecipesController < ApplicationController
-
-  def index
-  end
+  def index; end
 
   def show
-    @recipe=Recipe.find(params[:id])
-    @recipe_items=@recipe.recipe_foods
+    @recipe = Recipe.find(params[:id])
+    @recipe_items = @recipe.recipe_foods
   end
 
-  def create
-  end
+  def create; end
 
-  def destroy
-
-  end
+  def destroy; end
 
   def togle
-     @recipe=Recipe.find(params[:id])
-    @recipe.public=!@recipe.public
+    @recipe = Recipe.find(params[:id])
+    @recipe.public = !@recipe.public
     @recipe.save
     redirect_to @recipe
   end
