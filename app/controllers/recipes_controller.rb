df4248f36e4b0bd3f4d9fa205c999db6c075c595
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-12-02 12:04:23 +0100

    add add_recipe_form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,13 +2,26 @@ class RecipesController < ApplicationController
   def index;
   @current_user_recipes=current_user.recipes
   end
+  
+  def new;
+  @recipe=Recipe.new
+  end
+
 
   def show
     @recipe = Recipe.find(params[:id])
     @recipe_items = @recipe.recipe_foods
   end
 
-  def create; end
+  def create
+    @recipe=Recipe.new(recipe_params)
+    @recipe.user_id=current_user.id
+    if @recipe.save
+    redirect_to recipes_path
+    else
+        render :new, status: :unprocessable_entity
+    end
+  end
 
   def destroy
     @recipe = Recipe.find(params[:id])
@@ -26,5 +39,11 @@ class RecipesController < ApplicationController
     @recipe.public = !@recipe.public
     @recipe.save
     redirect_to @recipe
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name,:preparation_time,:cooking_time,:description,:public)
   end
 end
